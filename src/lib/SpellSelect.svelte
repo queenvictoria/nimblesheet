9e820fdad752de1f287a32268126ca2d61c9d87f
Snow@Moana <snow@hol.ly>
2026-06-14 11:06:56 +1000

    Hide spells section if it's empty.

--- src/lib/SpellSelect.svelte
+++ src/lib/SpellSelect.svelte
@@ -134,9 +134,10 @@
 	let maxMana = $derived(getMaxMana(manaRecipe[charClass]));
 </script>
 
-<Card.Root>
-	<Card.Header>
-		<div class="flex items-center gap-2">
+{#if !locked || available.length > 0}
+	<Card.Root>
+		<Card.Header>
+			<div class="flex items-center gap-2">
 			<Card.Title class="flex grow flex-row items-center gap-2 text-lg">
 				<span>Spells</span>
 
@@ -255,3 +256,4 @@
 		{/each}
 	</Card.Content>
 </Card.Root>
+{/if}
